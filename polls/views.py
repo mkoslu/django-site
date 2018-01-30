788e3268e3785a9bdecac44ed0069aa2e0e3ef74
Memet Koslu <memet@startmail.com>
2018-01-30 14:57:17 +0300

    Database Setup

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
-<<<<<<< HEAD
-<<<<<<< HEAD
 from django.http import HttpResponse
-=======
->>>>>>> df579e5dc27786635d13f2dcd7fc690b985f823b
-=======
-from django.http import HttpResponse
->>>>>>> 4e213486b4a26bfc6ae50a195eb61e5d50242ffe
 
 # Create your views here.
 def index(request):
